073c2b79b5c7adc8a738d86b1548cd1cb32af905
Isaac Jo <isaacjo91@gmail.com>
2017-03-12 16:27:23 -0700

    timeout

--- index.js
+++ index.js
@@ -5,23 +5,41 @@ var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 var Q = require('q');
 var app = express();
-
-var connection = mysql.createConnection({
+var db_config = mysql.createConnection({
   //properties
   host : 'us-cdbr-iron-east-03.cleardb.net',
   user : 'bc5d91c9f9e722',
   password : '30ea785d',
   database : 'heroku_8ca4fc2f56bc78e',
 });
+var connection;
 
 
-connection.connect(function (error) {
-    if (!!error){
-      console.log('Error');
-    } else {
-      console.log('Connected');
+
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
     }
-});
+  });
+}
+
+handleDisconnect();
+
 
 
 app.set('port', (process.env.PORT || 5000));
